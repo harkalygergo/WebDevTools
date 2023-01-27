dfd18658c8cccafff3d511b25f92ac041a461bab
Harkály Gergő <git@harkalygergo.hu>
2023-01-27 15:22:09 +0100

    Note class created

--- notes/note.php
+++ notes/note.php
@@ -4,3 +4,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+class Note
+{
+    public function __construct()
+    {
+        // do nothing
+    }
+
+    public function init()
+    {
+
+    }
+}
